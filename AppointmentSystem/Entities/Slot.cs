20a9d9185bd5a0711dbc54818f1166fdc32556da
kristabelk <kristabelkhoo@gmail.com>
2023-06-19 10:25:23 +0800

    created entities

---
+++ AppointmentSystem/Entities/Slot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentSystem.Entities
+{
+    public class Slot
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; } //or string? not sure.. tbc
+        public Guid DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public bool IsReserved { get; set; }
+        public decimal Cost { get; set; }
+
+    }
+}
